afa63e4e9ac4e8de88ddfa7014a8d7f004a44756
Diana217 <72453701+Diana217@users.noreply.github.com>
2024-04-18 12:02:13 +0300

    Add files via upload

--- Sequential/Sequential.cpp
+++ Sequential/Sequential.cpp
@@ -24,7 +24,7 @@ void quick_sort(std::vector<int>& arr, int left, int right) {
 }
 
 int main() {
-    std::ifstream infile("C:\\Users\\dulko\\source\\repos\\FileGeneration\\input3.txt");
+    std::ifstream infile("C:\\Users\\dulko\\source\\repos\\FileGeneration\\input20.txt");
     if (!infile.is_open()) {
         std::cerr << "Failed to open input file." << std::endl;
         return 1;
@@ -46,18 +46,6 @@ int main() {
     auto end = std::chrono::steady_clock::now();
     auto diff = end - start;
     std::cout << "Sorting time: " << std::chrono::duration <double, std::milli>(diff).count() << " ms" << std::endl;
-
-    std::ofstream outfile("sorted_output1.txt");
-    if (!outfile.is_open()) {
-        std::cerr << "Failed to open output file." << std::endl;
-        return 1;
-    }
-    for (int num : arr) {
-        outfile << num << " ";
-    }
-    outfile.close();
-
-    std::cout << "Sorted numbers written to file" << std::endl;
 
     return 0;
 }
